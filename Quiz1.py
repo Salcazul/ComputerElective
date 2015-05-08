4689a8056ddf7d337aa77f48465b50bb13d9d69f
Salcazul <salcazul@gmail.com>
2015-05-08 08:23:30 -0500

    Create Quiz1.py

---
+++ Quiz1.py
@@ -0,0 +1,9 @@
+# assigning variables
+sideA = 12.55
+sideB = 17.85
+# importing math
+import math
+# performing operation
+sideC = math.sqrt((sideA*sideA)+(sideB*sideB))
+# printing sideC
+print sideC
