016c4b5cb68bd8cd3f940c31e775190a202bedf0
Salcazul <salcazul@gmail.com>
2015-05-08 08:25:04 -0500

    Create Variables2.py

---
+++ Variables2.py
@@ -0,0 +1,17 @@
+mystring = "hello"
+myfloat = 10.0
+myint = 20
+# Print your variables
+print mystring, myfloat, myint
+# Concactenate your variables
+print mystring+'.', 'I am robot number', myint, 'and my software is of version', myfloat
+# Perform five operations with your number variables
+print myfloat + myint
+print myfloat * 2 + 4
+print myint * 4 + 100
+print myint * ( 4 + 100 )
+print ( myint - myfloat ) * 1.2 * 12
+# Perform more complex operations by importing math
+import math
+root = math.sqrt(16)
+print root
