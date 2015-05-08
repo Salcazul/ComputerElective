fd5f7e685df581b76c0a61c1c808cd9959201031
Salcazul <salcazul@gmail.com>
2015-05-08 08:24:35 -0500

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,23 @@
+name = 'Salvador Alvarado'
+age = 16
+gpa = 4.0
+
+print name
+print age
+print gpa
+
+print 'My name is', name+'.', 'I am', age, 'years old. My GPA is', gpa
+
+print "In ten years, I will be", age + 10
+
+#Dog years
+ageInHumanYears = 5
+ageInDogYears = ageInHumanYears * 7
+print 'My dog is', ageInDogYears, 'years old in dog years.'
+print 'But my dog is', ageInHumanYears, 'years old in human years.'
+
+myNumber = 2.0 * 5 + 3
+print myNumber
+
+numberOne = 4 * 5 + 5
+print numberOne
