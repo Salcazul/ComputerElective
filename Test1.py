ca3ce151ac74126e773a34dad0fd084b82b406bb
Salcazul <salcazul@gmail.com>
2015-05-08 08:25:37 -0500

    Create Test1.py

---
+++ Test1.py
@@ -0,0 +1,11 @@
+operand1 = 95
+operand2 = 64.95
+
+print "95 + 64.5 =", operand1 + operand2
+print "95 - 64.5 =", operand1 - operand2
+print "95 * 64.5 =", operand1 * operand2
+print "95 / 64.5 =", operand1 / operand2
+print "95 % 64.5 =", operand1 % operand2
+
+# My solution for Step 16 is the best because it uses concoction to
+# print the equation being solved, and then variables to print the answer.
