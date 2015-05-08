84ab94e22fe1429a75a20083ca8a63a421d5dd8b
Salcazul <salcazul@gmail.com>
2015-05-08 08:22:56 -0500

    Create Practice1.py

---
+++ Practice1.py
@@ -0,0 +1,17 @@
+name = 'Salvador'
+lastname = 'Alvarado'
+address = 'Puerto Azul Mz. A3 V14'
+email = 'salcazul@gmail.com'
+phonenumber = '0986080645'
+
+print name
+print lastname
+print address
+print email
+print phonenumber
+
+print 'My name is', name, lastname+'.', 'I live in', address+'.', 'My email is', email+'.', 'My phone number is', phonenumber+'.'
+
+favmov = "Moonrise Kingdom"
+releaseyear = 2012
+imdbrat = 7.8
