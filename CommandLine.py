8dc85b072876a8827e55804458aca68fce4a4bcd
Salcazul <salcazul@gmail.com>
2015-05-12 09:02:53 -0500

    Create CommandLine.py

---
+++ CommandLine.py
@@ -0,0 +1,6 @@
+import sys
+# print "You entered these arguments", sys.argv
+# print "The first argument you entered is: ", sys.argv[1]
+# print "The second argument you entered is: ", sys.argv[2]
+# print "The third argument you entered is: ", sys.argv[3]
+print 'My name is', sys.argv[1]
