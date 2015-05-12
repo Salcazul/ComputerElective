004ba7f6ce4a9032c0ddee4e0126cbcd96790606
Salcazul <salcazul@gmail.com>
2015-05-12 09:02:24 -0500

    Create Practice2.py

---
+++ Practice2.py
@@ -0,0 +1,7 @@
+import sys
+print "Name: ", sys.argv[1]
+print "Last Name: ", sys.argv[2]
+print "Year of Brith: ", sys.argv[3]
+print "Favorite Singer: ", sys.argv[4]
+print "Favorite Movie: ", sys.argv[5]
+print "Best Friend: ", sys.argv[6]
