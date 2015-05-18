c17c4dc980e6ed65e02445a5584bc6e2bace5b6f
Salcazul <salcazul@gmail.com>
2015-05-18 08:06:40 -0500

    Create Practice3.py

---
+++ Practice3.py
@@ -0,0 +1,25 @@
+#Create a form with the following info
+#Name
+name = raw_input("What is your name?")
+
+#LastName
+lastname = raw_input("What is your last name?")
+
+#Year of birth
+year = input("What is your year of birth?")
+
+#Favorite singer/band
+music = raw_input("What is your favorite singer/band?")
+
+#Favorite movie
+movie = raw_input("What is your favorite movie?")
+
+#Best friend
+friend = raw_input("Who is your best friend?")
+
+#Calculate age in 10 years
+age = (2015 - year) + 10
+print "You are", 2015 - year, "years old. In ten years you will be", age, "years old."
+
+#Print name 10 times
+print name * 10
