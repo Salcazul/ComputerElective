732900813a27e2d64a9358d5efe98b5ab14c4e6b
Salcazul <salcazul@gmail.com>
2015-05-26 09:35:56 -0500

    Create Conditionals2.py

---
+++ Conditionals2.py
@@ -0,0 +1,8 @@
+age=input("What is the customer's age?")
+if age >= 21:
+    print "The customer is legally able to consume liquor."
+    print "You may serve."
+else:
+    print "The customer is not legally able to consume liquor."
+    print "You may not serve."
+print "End of program."
